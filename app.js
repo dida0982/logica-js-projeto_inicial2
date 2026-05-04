bfe49441015dae6c224779241378c2f02d698a0f
dida0982 <dida0982@gmail.com>
2026-05-04 15:33:45 -0300

    feat: add a function that sum numbers
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-// Criar uma função que exibe "Olá, mundo!" no console.
+//1 Criar uma função que exibe "Olá, mundo!" no console.
 
-// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+//2 Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
 
-// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+//3 Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
 
-// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+//4 Criar uma função que recebe três números como parâmetros e retorna a média deles.
 
-// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+//5 Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
 
-// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+//6 Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
 
 
-function lookYouConsole (  )
-{
-    let name = prompt(`What your name?`)
-    console.log(`Hello ${name}!`)
+function sumOfNumbers() {
+    let number1 = Number( prompt( `Enter a number:` ));
+    let number2 = Number( prompt( `Enter another number:` ));
+    let number3 =  number1 + number2;
+
+    console.log(`${number1} + ${number2} = ${number3}`);
 }
 
-lookYouConsole();
+sumOfNumbers()
